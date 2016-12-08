cdf5b45dc31213ca06f9c49b78b41190764e7df3
张正来 <zhenglaizhang@hotmail.com>
2016-12-08 17:07:06 +0800

    add notes about making ubuntu bootable usb

--- provision.sh
+++ provision.sh
@@ -1,2 +1,19 @@
 
 pip install pgcli
+
+
+# make bootable disk with gddrescue
+apt-get install gddrescue
+fdisk -l
+# take care to grap the device name of the usb disk
+ddrescue -d -D --force <path and file name of the ISO> /dev/<device name>
+
+# make bootable disk with dd
+dd if=/home/user/downloads/ubuntu-16.04-server-amd64.iso of=/dev/sdc bs=1M; sync
+
+
+# Mac
+# hdiutil convert -format UDRW -o <path_to_save_IMG_file> <path_to_ISO_image> # convert img to iso
+# diskutil list
+# diskutil unmountDisk /dev/disk2
+# # sudo dd if=<path_to_IMG_file> of=<device_node_of_usb_media> bs=1m
