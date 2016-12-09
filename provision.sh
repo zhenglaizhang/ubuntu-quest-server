31eb10927c438cec85fa16dd3a87a06c77be79a3
张正来 <zhenglaizhang@hotmail.com>
2016-12-09 23:07:36 +0800

    apt install sbt

--- provision.sh
+++ provision.sh
@@ -86,7 +86,7 @@ sudo apt-get install scala scala-doc
 
 # sbt 
 echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
 sudo apt-get update
 sudo apt-get install sbt
 
