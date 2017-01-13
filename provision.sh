867a07a06f94bf2850d6a4e780f45a0b421bc9b9
张正来 <zhenglaizhang@hotmail.com>
2017-01-13 14:41:35 +0800

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -162,3 +162,22 @@ sudo apt-get autoremove   // 删除依赖包，不再使用的package
 sudo apt-get -f install   // 尝试修正安装过程中出现的依赖性关系
 sudo add-apt-repository ppa:fcitx-team/nightly
 http://pinyin.sogou.com/linux/?r=pinyin
+
+
+
+
+
+# ELK
+# java should be installed first
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
+sudo apt-get update && sudo apt-get install elasticsearch
+sudo /bin/systemctl daemon-reload
+sudo /bin/systemctl enable elasticsearch.service
+sudo systemctl start elasticsearch.service
+sudo systemctl stop elasticsearch.service
+
+sudo apt-get install kibana
+sudo /bin/systemctl daemon-reload
+sudo /bin/systemctl enable kibana.service
