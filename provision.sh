160b639d094e2d31d090f2198f026571086d03a3
张正来 <zhenglaizhang@hotmail.com>
2017-02-27 14:52:34 +0800

    docker updated

--- provision.sh
+++ provision.sh
@@ -152,6 +152,10 @@ sudo dpkg -i --force-depends google-chrome-stable_current_amd64.deb
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
 sudo apt-get update
+
+sudo apt-get install -y --no-install-recommends \
+    linux-image-extra-$(uname -r) \
+    linux-image-extra-virtual
 # install from docker repo instead of ubuntu repo
 apt-cache policy docker-engine
 sudo apt-get install -y docker-engine
